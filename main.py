154957c36def9d146a65581d40a269787f77a34d
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2026-04-17 09:39:17 -0300

    Feat: Class Artista - Ok

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 Base = declarative_base()
+
+class Artista(Base):
+    __tablename__ = "artistas"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+    genero = Column(String(50))
+    pais = Column(String(50))
+    biografia = Column(Text)
+
+    def __repr__(self):
+        return f"Artista: {self.id}, nome: {self.nome}, genero:{self.genero}, país: {self.pais}, biografia: {self.biografia})"
